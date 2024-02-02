55c1a7f840cb6bfe1f4a0089cc8c83fca4c693d6
Maksim <perminev.ma@gazprom-neft.ru>
2024-02-02 23:09:49 +0300

    fix: remove all_acces

--- network.tf
+++ network.tf
@@ -23,16 +23,3 @@ resource "vkcs_networking_router_interface" "intrface" {
    router_id = vkcs_networking_router.router.id
    subnet_id = vkcs_networking_subnet.subnetwork.id
 }
-
-# Manage a security group resource within VKCS
-resource "vkcs_networking_secgroup" "secgroup" {
-   name = "all_access"
-   description = "Open all port"
-}
-
-resource "vkcs_networking_secgroup_rule" "all_acc" {
-  description       = "Any inbound traffic from etcd hosts"
-  security_group_id = vkcs_networking_secgroup.secgroup.id
-  direction         = "ingress"
-  remote_ip_prefix  = "0.0.0.0/0"
-}
